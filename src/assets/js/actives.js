6f84ceaf5fe7ed0918db4d07eedc24cfae043a1d
rebron1900 <me@1900.live>
2024-05-12 21:38:03 +0800

    applist从云端请求，方便更新

--- src/assets/js/actives.js
+++ src/assets/js/actives.js
@@ -1,63 +1,66 @@
 // 定义WebSocket的URL
 const wsUrl = "wss://api.1900.live/actives_ws";
 const cdn = "https://cdn.1900.live/apps/";
+const appUrl = "https://1900.live/assets/app.json";
 
-const app = {
-    wechat: {
-        title: "微信",
-        url: "wechat.png",
-        action: "摸鱼",
-    },
-    chrome: {
-        title: "Chrome",
-        url: "chrome.png",
-        action: "冲浪",
-    },
-    code: {
-        title: "VSCODE",
-        url: "code.png",
-        action: "编码",
-    },
-    "lx-music-desktop.exe": {
-        title: "洛雪音乐播放器",
-        url: "music.png",
-        action: "听音乐",
-    },
-    tim: {
-        title: "TIM",
-        url: "qq.png",
-        action: "摸鱼",
-    },
-    hydee: {
-        title: "工作ERP",
-        url: "hydee.jpg",
-        action: "工作",
-    },
-    wxwork: {
-        title: "企业微信",
-        url: "wxwork.png",
-        action: "工作",
-    },
-    obsidian: {
-        title: "Obsidian",
-        url: "obsidian.png",
-        action: "做笔记",
-    },
-    windowsterminal: {
-        title: "WindowsTerminal",
-        url: "windowsterminal.png",
-        action: "",
-    },
-};
+let app = {};
+// {
+//     wechat: {
+//         title: "微信",
+//         url: "wechat.png",
+//         action: "摸鱼",
+//     },
+//     chrome: {
+//         title: "Chrome",
+//         url: "chrome.png",
+//         action: "冲浪",
+//     },
+//     code: {
+//         title: "VSCODE",
+//         url: "code.png",
+//         action: "编码",
+//     },
+//     "lx-music-desktop.exe": {
+//         title: "洛雪音乐播放器",
+//         url: "music.png",
+//         action: "听音乐",
+//     },
+//     tim: {
+//         title: "TIM",
+//         url: "qq.png",
+//         action: "摸鱼",
+//     },
+//     hydee: {
+//         title: "工作ERP",
+//         url: "hydee.jpg",
+//         action: "工作",
+//     },
+//     wxwork: {
+//         title: "企业微信",
+//         url: "wxwork.png",
+//         action: "工作",
+//     },
+//     obsidian: {
+//         title: "Obsidian",
+//         url: "obsidian.png",
+//         action: "做笔记",
+//     },
+//     windowsterminal: {
+//         title: "WindowsTerminal",
+//         url: "windowsterminal.png",
+//         action: "",
+//     },
+// };
 
 // 保存WebSocket实例的变量
 let ws2;
 let counter = 0;
 
-// 定义重连的函数
-function reconnect() {
-    // 尝试重新连接
+// 初始化WebSocket连接
+export default function initWebSocket() {
     ws2 = new WebSocket(wsUrl);
+
+    updateAppList();
     // 绑定事件处理函数
     ws2.onopen = onOpen;
     ws2.onmessage = onMessage;
@@ -65,14 +68,13 @@ function reconnect() {
     ws2.onerror = onError;
 }
 
-// 初始化WebSocket连接
-export default function initWebSocket() {
-    ws2 = new WebSocket(wsUrl);
-    // 绑定事件处理函数
-    ws2.onopen = onOpen;
-    ws2.onmessage = onMessage;
-    ws2.onclose = onClose;
-    ws2.onerror = onError;
+function updateAppList(){
+    fetch(appUrl).then((rep) => {
+        rep.json().then((data) => {
+            // 绑定事件处理函数
+            app = data;
+        });
+    });
 }
 
 // 连接成功的处理函数
@@ -125,7 +127,7 @@ function onMessage(event) {
 function onClose(event) {
     // 尝试重新连接
     document.querySelector(".actives").classList.add("exit");
-    setTimeout(reconnect, 5000); // 5秒后尝试重新连接
+    setTimeout(initWebSocket, 5000); // 5秒后尝试重新连接
 }
 
 // 连接错误的处理函数
